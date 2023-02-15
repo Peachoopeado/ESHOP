fc840dce4a897b67c735316a8da25d2b9d94625e
Кирилл <iorbit4557@gmail.com>
2023-02-15 23:56:50 +0300

    Реализована фильтрация по категориям2

---
+++ shop/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from .models import Category, Product, OilType, Viscosity, Compound, Fuel
+
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class OilTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(OilType, OilTypeAdmin)
+
+
+class ViscosityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Viscosity, ViscosityAdmin)
+
+class CompoundAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Compound, CompoundAdmin)
+
+class FuelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Fuel, FuelAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'oilType', 'viscosity', 'compound', 'fuel', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'oilType', 'viscosity', 'compound', 'fuel']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
