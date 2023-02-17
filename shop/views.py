f713058d540364e28afd88c98836037ebcc4030c
Кирилл <iorbit4557@gmail.com>
2023-02-17 15:32:47 +0300

    Работа над фильтром

--- shop/views.py
+++ shop/views.py
@@ -4,14 +4,23 @@ from .models import Category, Product, OilType, Viscosity, Compound, Fuel
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
+    oiltype = OilType.objects.all()
+    viscosity = Viscosity.objects.all()
+    compound = Compound.objects.all()
+    fuel = Fuel.objects.all()
     products = Product.objects.filter(available=True)
 
     if category_slug:
         category = get_object_or_404(Category, slug = category_slug)
         products = products.filter(category=category)
+
     data = {
         'category': category,
         'categories': categories,
+        'oiltype': oiltype,
+        'viscosity': viscosity,
+        'compound': compound,
+        'fuel': fuel,
         'products': products
     }
     return render(request, 'shop/product/list.html', data)
